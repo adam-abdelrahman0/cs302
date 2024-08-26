2489212f9013fa692f02f89413d3567034f61d81
Adam A Abdelrahman <aabdelr5@tesla8.eecs.utk.edu>
2024-08-25 22:54:16 -0400

    add artist struct and iterator

--- project1/io_start.cpp
+++ project1/io_start.cpp
@@ -36,6 +36,7 @@ int main(int argc, char* argv[]) {
     int curTime;
 
     vector<Song> tempSongs;
+    map<string, Artist> artists;
 
     musicFile.open(argv[1]);
 
@@ -48,18 +49,43 @@ int main(int argc, char* argv[]) {
         stringstream ss;
         ss << line;
 
+        // song title
         getline(ss, currentLineString, ' ');
         replace(currentLineString.begin(), currentLineString.end(), '_', ' ');
         currentSong.title = currentLineString;
 
+        // song duration
         getline(ss, currentLineString, ' ');
         currentSong.time = currentLineString;
+
+        // song artist (add song to album and album to artist)
+        getline(ss, currentLineString, ' ');
+
+        artists[currentLineString].name = currentLineString;
+        //artists[currentLineString].time = currentSong.time;
+        artists[currentLineString].nsongs++;
+
+        getline(ss, currentTempString, ' ');
+        artists[currentLineString].albums[currentTempString].name = currentTempString;
+        //artists[currentLineString].albums[currentTempString].time = currentSong.time;
+        artists[currentLineString].albums[currentTempString].nsongs++;
+
+        //artists[currentLineString].albums[currentTempString].songs[currentSong.title].title = currentSong.title;
+        //artists[currentLineString].albums[currentTempString].songs[currentSong.title].time = currentSong.time;
+
 
         tempSongs.push_back(currentSong);
     }
     
     for (int i = 0; i < tempSongs.size(); i++) {
         cout << tempSongs.at(i).title << " " << tempSongs.at(i).time << endl;
+    }
+
+    cout << "\n\n---------- testing structs ----------\n\n" << endl;
+    
+    for (auto itr = artists.begin(); itr != artists.end(); ++itr) {
+        cout << '\t' << itr->first << '\t' << itr->second.name
+             << '\n';
     }
     musicFile.close();
 
