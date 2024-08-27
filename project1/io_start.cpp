bcc0fd5f70fb4bec72c50e02655cd3f9b4629569
WellsAndrew <awells48@vols.utk.edu>
2024-08-27 14:55:11 -0400

    Adding time enumeration

--- project1/io_start.cpp
+++ project1/io_start.cpp
@@ -2,6 +2,7 @@
 #include <string>
 #include <fstream>
 #include <sstream>
+#include <iomanip>
 #include <vector>
 #include <algorithm>
 #include <map>
@@ -28,9 +29,43 @@ struct Artist {
     int nsongs;
 };
 
+string str_time(int seconds) {
+	string time_out, hh, mm, ss;
+	int hours = 0;
+	int minutes = 0;
+
+	hours = seconds / 3600;
+	seconds -= hours * 3600;
+	minutes = seconds / 60;
+	seconds -= minutes * 60;
+
+	hh = to_string(hours);
+	mm = to_string(minutes);
+	ss = to_string(seconds);
+
+	stringstream time_stream;
+
+	if (hours > 0) {
+		time_stream << setfill('0') << setw(2) << hh << ":";
+	}
+	if (minutes > 0 && hours == 0) {
+		time_stream  << mm << ":";
+	}
+		else if (minutes > 0 && hours > 0) {
+			time_stream << setfill('0') << setw(2) << hh << ":";
+		}
+	if (seconds > 0){
+		time_stream << setfill('0') << setw(2) << ss;
+	}
+
+	time_out = time_stream.str();
+	return time_out;
+}
+
 int main(int argc, char* argv[]) {
     ifstream musicFile;
-    string line, currentLineString, artistName, albumName, songName, trackNum;
+    string line, currentLineString, artistName, albumName, songName, trackNum, min, sec;
+	int song_time;
 
     vector<Song> tempSongs;
     map<string, Artist> artists;
@@ -50,23 +85,29 @@ int main(int argc, char* argv[]) {
         getline(ss, songName, ' ');
         replace(songName.begin(), songName.end(), '_', ' ');
         currentSong.title = songName;
+		
+		// song time
+        getline(ss, min, ':');
+		getline(ss, sec, ' ');
+		//Convert to total seconds
+		song_time = (stoi(sec) + (60 * stoi(min)));
 
-        // song duration (TODO: convert to total seconds)
-        getline(ss, currentLineString, ' ');
-        currentSong.time = currentLineString;
+//		cout << song_time << endl;
+
+		currentSong.time = str_time(song_time);
 
         // song artist (add song to album and album to artist)
         getline(ss, artistName, ' ');
         replace(artistName.begin(), artistName.end(), '_', ' ');
 
         artists[artistName].name = artistName;
-        //artists[currentLineString].time += currentSong.time;
+        artists[artistName].time += song_time;
         artists[artistName].nsongs++;
 
         getline(ss, albumName, ' ');
         replace(albumName.begin(), albumName.end(), '_', ' ');
         artists[artistName].albums[albumName].name = albumName;
-        //artists[currentLineString].albums[currentTempString].time = currentSong.time;
+        artists[artistName].albums[albumName].time += song_time;
         artists[artistName].albums[albumName].nsongs++;
 
         getline(ss, currentLineString, ' ');
@@ -77,24 +118,24 @@ int main(int argc, char* argv[]) {
         artists[artistName].albums[albumName].songs[trackNum].time = currentSong.time;
 
 
-        tempSongs.push_back(currentSong);
+//        tempSongs.push_back(currentSong);
     }
     
-    for (int i = 0; i < tempSongs.size(); i++) {
-        cout << tempSongs.at(i).title << " " << tempSongs.at(i).time << endl;
-    }
+//    for (int i = 0; i < tempSongs.size(); i++) {
+//        cout << tempSongs.at(i).title << " " << tempSongs.at(i).time << endl;
+//    }
 
-    cout << "\n\n---------- testing structs ----------\n\n" << endl;
+//    cout << "\n\n---------- testing structs ----------\n\n" << endl;
     
     for (auto itr = artists.begin(); itr != artists.end(); ++itr) {
-        cout << itr->first << ": " << itr->second.nsongs << ", " << endl;
+        cout << itr->first << ": " << itr->second.nsongs << ", " << str_time(itr->second.time) << endl;
 
         for (auto albumsItr = itr->second.albums.begin(); albumsItr != itr->second.albums.end(); ++albumsItr) {
-            cout << "\t" << albumsItr->first << ": " << albumsItr->second.nsongs << endl;
+            cout << "\t" << albumsItr->first << ": " << albumsItr->second.nsongs << ", " << str_time(albumsItr->second.time) << endl;
 
             for (auto songsItr = albumsItr->second.songs.begin(); songsItr != albumsItr->second.songs.end(); ++songsItr) {
-            cout << "\t \t" << songsItr->first << ": " << songsItr->second.title << endl;
-        }
+            	cout << "\t \t" << songsItr->first << ": " << songsItr->second.title << " " << songsItr->second.time << endl;
+        	}
         }
     }
     musicFile.close();
